99384d8144dd63a8afc431bc808fae77b4f7b7a7
HerculesGit <hercules.silva@dce.ufpb.br>
2018-10-24 21:36:59 -0300

    Voltando com control+Z tirando o FragmentLayout não comitado

---
+++ app/src/main/java/com/dominandoandroid/example/hercules/e_moto/FinalizarCadastro.java
@@ -0,0 +1,14 @@
+package com.dominandoandroid.example.hercules.e_moto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FinalizarCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_finalizar_cadastro);
+    }
+}
